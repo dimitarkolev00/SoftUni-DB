337801f250de7d008546a86919728900ec6c3f73
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-08-02 21:05:56 +0300

    Initial push

---
+++ Entity Framework Core/Exam Preparation/13-Dec-2019/Data/Models/AuthorBook.cs
@@ -0,0 +1,17 @@
+﻿namespace BookShop.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    public class AuthorBook
+    {
+        [Required]
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
